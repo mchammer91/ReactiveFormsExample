db058ca81950a1ca99d26915e3d08205a92e8c9e
Michael.Canova <1michaelcanova@gmail.com>
2019-01-27 21:02:29 -0600

    finished adding reactive contact component

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { PersonalData, ContactRequest } from 'src/models/contact-request';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+
+export class ContactComponent implements OnInit {
+
+  countries = ['USA', 'Germany', 'Italy', 'France'];
+  requestTypes = ['Claim', 'Feedback', 'Help Request'];
+  contactForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.createFormGroup();
+   }
+
+  ngOnInit() {
+  }
+
+  createFormGroup() {
+    return new FormGroup({
+      personalData: new FormGroup({
+        email: new FormControl(),
+        mobile: new FormControl(),
+        country: new FormControl()
+      }),
+      requestType: new FormControl(),
+      text: new FormControl()
+    });
+  }
+
+  createFormGroupWithBuilder(formBuilder: FormBuilder) {
+    return formBuilder.group({
+      personalData: formBuilder.group(new PersonalData()),
+      requestType: '',
+      text: ''
+    });
+  }
+
+  onSubmit() {
+    const RESULT: ContactRequest = Object.assign({}, this.contactForm.value);
+    RESULT.personalData = Object.assign({}, RESULT.personalData);
+
+    console.log(RESULT);
+  }
+  revert() {
+    // resets to a blank object
+    this.contactForm.reset();
+
+    // resets to the provided model
+    this.contactForm.reset({
+      personalData: new PersonalData(),
+      requestType: '',
+      text: ''
+    });
+  }
+}
